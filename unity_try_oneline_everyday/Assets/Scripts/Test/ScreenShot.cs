6e54faf015152f3d6da63512dab4eaa9f265b59f
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2022-05-26 23:18:13 +0900

    Testに画面スクリーンショットのスクリプトを追加(エディターの場合Assets直下にディレクトリが作られる)

---
+++ unity_try_oneline_everyday/Assets/Scripts/Test/ScreenShot.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Test
+{
+    public class ScreenShot : MonoBehaviour
+    {
+        [Header("保存先フォルダ名を指定")]
+        [SerializeField]
+        public string FolderName = "screen_shot";
+
+        private bool isCreating = false;
+        private string path = default;
+
+        private void Start()
+        {
+            path = Application.dataPath + "/" + FolderName + "/";    
+        }
+
+        public void PrintScreen()
+        {
+            StartCoroutine(CreateScreenShot());
+        }
+
+        IEnumerator CreateScreenShot()
+        {
+            if (isCreating)
+            {
+                yield break;
+            }
+
+            Debug.Log("<color=cyan>" + "Start CreateScreenShot" + "</color>");
+
+            isCreating = true;
+
+            yield return null;
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string date = DateTime.Now.ToString("yy-MM-dd_HH-mm-ss");
+            string fileName = path + date + ".png";
+
+            ScreenCapture.CaptureScreenshot(fileName);
+
+            yield return new WaitUntil(() => File.Exists(fileName));
+
+            isCreating = false;
+
+            Debug.Log("<color=cyan>" + "Finished CreateScreenShot" + "</color>");
+        }
+    }
+}
