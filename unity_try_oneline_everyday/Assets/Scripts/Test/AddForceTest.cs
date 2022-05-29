7d580df2fc232a337a40b67125357f8d1940be52
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2022-05-29 23:07:22 +0900

    RigidbodyのAddForceのテスト

---
+++ unity_try_oneline_everyday/Assets/Scripts/Test/AddForceTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class AddForceTest : MonoBehaviour
+    {
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            var rb = this.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.AddForce(new Vector2(0.75f, 0.0f));
+            }
+        }
+    }
+}
---
+++ unity_try_oneline_everyday/Assets/Scripts/Test/TransactionTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class TransactionTest : MonoBehaviour
+    {
+        private bool isActionShrink = default;
+        private bool isActionExpansion = default;
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void OnMouseDown()
+        {
+
+        }
+
+        private IEnumerator ShrinkScaleTransaction()
+        {
+            float minScaleValue = 0.5f;
+            float maxScaleValue = 1.0f;
+
+
+
+            yield return true;
+        }
+
+        private IEnumerator ExpansionkScaleTransaction()
+        {
+            float minScaleValue = 1.0f;
+            float maxScaleValue = 1.5f;
+
+
+
+            yield return true;
+        }
+    }
+}
