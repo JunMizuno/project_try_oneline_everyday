a19010c703f056a355d58738b1503e0163b79d41
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2022-05-30 23:38:50 +0900

    ボタンアクションなどで使えそうなトランザクションをコルーチンで実装

--- unity_try_oneline_everyday/Assets/Scripts/Test/TransactionTest.cs
+++ unity_try_oneline_everyday/Assets/Scripts/Test/TransactionTest.cs
@@ -21,25 +21,68 @@ namespace Test
 
         private void OnMouseDown()
         {
-
+            StartCoroutine(ShrinkScaleTransaction());
         }
 
         private IEnumerator ShrinkScaleTransaction()
         {
+            if (isActionShrink)
+            {
+                yield break;
+            }
+
+            isActionShrink = true;
+
             float minScaleValue = 0.5f;
             float maxScaleValue = 1.0f;
 
+            var scale = this.gameObject.transform.localScale;
 
+            // @todo.mizuno スピード感は要調整。
+            while (scale.x > minScaleValue)
+            {
+                scale.x -= 0.01f;
+                scale.y -= 0.01f;
+                scale.z -= 0.01f;
+                this.gameObject.transform.localScale = scale;
+
+                yield return new WaitForSeconds((1.0f / 60.0f));
+            }
+
+            while (scale.x < maxScaleValue)
+            {
+                scale.x += 0.01f;
+                scale.y += 0.01f;
+                scale.z += 0.01f;
+                this.gameObject.transform.localScale = scale;
+
+                yield return new WaitForSeconds((1.0f / 60.0f));
+            }
+
+            this.gameObject.transform.localScale = new Vector3(maxScaleValue, maxScaleValue, maxScaleValue);
+
+            isActionShrink = false;
 
             yield return true;
         }
 
         private IEnumerator ExpansionkScaleTransaction()
         {
+            if (isActionExpansion)
+            {
+                yield break;
+            }
+
+            isActionExpansion = true;
+
             float minScaleValue = 1.0f;
             float maxScaleValue = 1.5f;
 
 
+
+
+
+            isActionExpansion = false;
 
             yield return true;
         }
--- unity_try_oneline_everyday/Assets/Shaders/Test/Flags.shader
+++ unity_try_oneline_everyday/Assets/Shaders/Test/Flags.shader
@@ -40,7 +40,7 @@ Shader "Unlit/Flags"
                 o.uv = TRANSFORM_TEX(v.uv, _MainTex);
                 float add = 0.5f * sin(_Time * 100 + v.vertex.x * 100);
                 // Y軸を操作すると波打つ動き。
-                //o.vertex.xyz = float3(o.vertex.x, o.vertex.y + add, o.vertex.z);
+                o.vertex.xyz = float3(o.vertex.x, o.vertex.y + add, o.vertex.z);
                 // Z軸を操作すると、-3を下回る場合は非表示になる。
                 //o.vertex.xyz = float3(o.vertex.x, o.vertex.y, o.vertex.z + add);
                 return o;
