4d712e89b138c25ded6e4b328c3f6c2e57d5d991
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2022-06-07 23:38:32 +0900

    RenderFeatureの基本の実装例を追加

---
+++ unity_try_oneline_everyday/Assets/Scripts/Test/Renderer/DownSamplingRenderFeature.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Test
+{
+    public class DownSamplingRenderFeature : ScriptableRendererFeature
+    {
+        private DownSamplingRenderPass renderPass;
+
+        /// <summary>
+        /// Passの作成
+        /// </summary>
+        public override void Create() => renderPass = new DownSamplingRenderPass();
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            // @memo.mizuno ScriptableRendererにPassを渡す。
+            // @memo.mizuno Passは複数登録もOK。
+            renderer.EnqueuePass(renderPass);
+        }
+    }
+}
---
+++ unity_try_oneline_everyday/Assets/Scripts/Test/Renderer/DownSamplingRenderPass.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Test
+{
+    public class DownSamplingRenderPass : ScriptableRenderPass
+    {
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            // @todo.mizuno 描画処理を実装
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// RenderPassEventで描画タイミングを指定。
+        /// </summary>
+        public DownSamplingRenderPass() => renderPassEvent = RenderPassEvent.AfterRenderingTransparents;
+    }
+}
