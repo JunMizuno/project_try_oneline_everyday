bafbe4a13588fcc3dc5fc23a3c540b54d13bfe08
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2022-06-09 23:15:10 +0900

    UniTaskの動作テスト

--- unity_try_oneline_everyday/Assets/Scripts/Test/CalcTest.cs
+++ unity_try_oneline_everyday/Assets/Scripts/Test/CalcTest.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+using Cysharp.Threading.Tasks;
+
 using UnityEngine;
 
 namespace Test
 {
     public class CalcTest : MonoBehaviour
     {
-        private void Start()
+        private async void Start()
         {
+            var str1 = await WaitTest(3);
 
+            Debug.Log("<color=cyan>" + "戻り値として受け取った値:" + str1 + "</color>");
+
+            var str2 = await WaitTest(1);
+
+            Debug.Log("<color=cyan>" + "戻り値として受け取った値:" + str2 + "</color>");
         }
 
         private void Update()
         {
 
+        }
+
+        private async UniTask<string> WaitTest(float waitTime)
+        {
+            Debug.Log("<color=cyan>" + "WaitTest開始 " + waitTime + "秒待機指定" + "</color>");
+
+            await UniTask.Delay((int)(waitTime * 1000));
+
+            return "WaitTest終了";
         }
     }
 }
