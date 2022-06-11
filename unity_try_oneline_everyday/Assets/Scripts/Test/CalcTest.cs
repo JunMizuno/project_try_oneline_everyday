f6d4d7c78449504aaa733718c5caced29742009c
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2022-06-11 23:08:20 +0900

    UniTaskのawaitのパターンを調査

--- unity_try_oneline_everyday/Assets/Scripts/Test/CalcTest.cs
+++ unity_try_oneline_everyday/Assets/Scripts/Test/CalcTest.cs
@@ -9,15 +9,9 @@ namespace Test
 {
     public class CalcTest : MonoBehaviour
     {
-        private async void Start()
+        private void Start()
         {
-            var str1 = await WaitTest(3);
-
-            Debug.Log("<color=cyan>" + "戻り値として受け取った値:" + str1 + "</color>");
-
-            var str2 = await WaitTest(1);
-
-            Debug.Log("<color=cyan>" + "戻り値として受け取った値:" + str2 + "</color>");
+            UniTaskTestAsync();
         }
 
         private void Update()
@@ -25,13 +19,54 @@ namespace Test
 
         }
 
-        private async UniTask<string> WaitTest(float waitTime)
+        private async void UniTaskTestAsync()
+        {
+            var str1 = await WaitTestAsync(3);
+
+            Debug.Log("<color=cyan>" + "戻り値として受け取った値:" + str1 + "</color>");
+
+            await WaitReturnTestAsync(2);
+
+            WaitVoidTestAsync(2).Forget();
+
+            var str2 = await WaitTestAsync(1);
+
+            Debug.Log("<color=cyan>" + "戻り値として受け取った値:" + str2 + "</color>");
+        }
+
+
+        private async UniTask<string> WaitTestAsync(float waitTime)
         {
             Debug.Log("<color=cyan>" + "WaitTest開始 " + waitTime + "秒待機指定" + "</color>");
 
             await UniTask.Delay((int)(waitTime * 1000));
 
             return "WaitTest終了";
+        }
+
+        private async UniTaskVoid WaitVoidTestAsync(float waitTime)
+        {
+            Debug.Log("<color=cyan>" + "WaitVoidTestAsync開始 " + waitTime + "秒待機指定" + "</color>");
+
+            await UniTask.Delay((int)(waitTime * 1000));
+
+            Debug.Log("<color=cyan>" + "WaitVoidTestAsync終了" + "</color>");
+        }
+
+        private async UniTask<bool> WaitReturnTestAsync(float waitTime)
+        {
+            Debug.Log("<color=cyan>" + "WaitReturnTestAsync開始 " + waitTime + "秒待機指定" + "</color>");
+
+            bool retValue = false;
+
+            await UniTask.Delay((int)(waitTime * 1000));
+            retValue = true;
+
+            await UniTask.WaitUntil(() => retValue);
+
+            Debug.Log("<color=cyan>" + "WaitReturnTestAsync終了" + "</color>");
+
+            return retValue;
         }
     }
 }
