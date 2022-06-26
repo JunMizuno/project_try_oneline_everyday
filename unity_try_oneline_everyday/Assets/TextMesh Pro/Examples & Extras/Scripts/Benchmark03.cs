1e65aa570f478bf74c0ddae360743b1e7e90e411
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2022-06-27 00:08:35 +0900

    TextMeshProのマテリアル追加

--- unity_try_oneline_everyday/Assets/TextMesh Pro/Examples & Extras/Scripts/Benchmark03.cs
+++ unity_try_oneline_everyday/Assets/TextMesh Pro/Examples & Extras/Scripts/Benchmark03.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.TextCore.LowLevel;
 
 
--- unity_try_oneline_everyday/Assets/TextMesh Pro/Examples & Extras/Scripts/TMP_TextSelector_A.cs
+++ unity_try_oneline_everyday/Assets/TextMesh Pro/Examples & Extras/Scripts/TMP_TextSelector_A.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 
 namespace TMPro.Examples
