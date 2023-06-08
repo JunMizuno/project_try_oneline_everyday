7189b3f886a6e543ab8e2fb773ee75507e36ff87
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2023-06-08 17:18:14 +0900

    スライダーバーの実装例

---
+++ unity_try_oneline_everyday/Assets/Scripts/Utility/CustomPointerEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Utility
+{
+    public class CustomPointerEvent : MonoBehaviour, IPointerUpHandler
+    {
+        [SerializeField]
+        public UnityEvent releasedEvent;
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            releasedEvent.Invoke();
+        }
+    }
+}
