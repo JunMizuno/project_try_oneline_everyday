485b4c44c12543b86c3f2e15af401143c1fbf41b
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2024-02-18 21:20:49 +0900

    ポストエフェクトの構成ファイルを追加

---
+++ unity_try_oneline_everyday/Assets/Scripts/PostEffect/SamplePostEffectProcessRenderFeature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+[Serializable]
+public class SamplePostEffectProcessRenderFeature : ScriptableRendererFeature
+{
+    [SerializeField] private Shader _shader;
+    [SerializeField] private PostprocessTiming _timing = PostprocessTiming.AfterOpaque;
+    [SerializeField] private bool _applyToSceneView = true;
+
+    private SamplePostEffectProcessRenderPass _postProcessPass;
+
+    public override void Create()
+    {
+        _postProcessPass = new SamplePostEffectProcessRenderPass(_applyToSceneView, _shader);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        _postProcessPass.Setup(renderer.cameraColorTarget, _timing);
+        renderer.EnqueuePass(_postProcessPass);
+    }
+}
---
+++ unity_try_oneline_everyday/Assets/Scripts/PostEffect/SamplePostEffectVolume.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[Serializable]
+[VolumeComponentMenu("SamplePostEffect")]
+public class SamplePostEffectVolume : VolumeComponent // VolumeComponentを継承する
+{
+    public bool IsActive() => tintColor != Color.white;
+
+    // Volumeコンポーネントで設定できる値にはxxxParameterクラスを使う
+    public ColorParameter tintColor = new ColorParameter(Color.white);
+}
